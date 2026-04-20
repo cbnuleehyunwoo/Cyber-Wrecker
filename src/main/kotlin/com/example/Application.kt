7ec789dacb39a8d2a20d69cc7b3b5a80e1e28083
cbnuLeehyunwoo <henow123@naver.com>
2026-04-20 21:08:25 +0900

    chore: 포트설정 수정

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -13,13 +13,14 @@ import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 8880, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
 fun Application.module() {
     install(ContentNegotiation) {
         json(Json {
+            ignoreUnknownKeys = true
             prettyPrint = true
             isLenient = true
         })
